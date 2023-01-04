f15ebf3f8df06e7c220eadce5c6f77a7acff65e6
Michael Wood <michaelwood33311@icloud.com>
2023-01-04 10:06:48 -0500

    added express https server

--- sendEmail.js
+++ sendEmail.js
@@ -1,6 +1,18 @@
+const listenPort = 5500;
 require('dotenv').config();
 const axios = require('axios');
+const privateKeyPath = '/etc/letsencrypt/live/node.pymnts.com/privkey.pem';
+const fullchainPath = '/etc/letsencrypt/live/node.pymnts.com/fullchain.pem';
 
+const express = require('express');
+const https = require('https');
+const cors = require('cors');
+const fs = require('fs');
+
+const app = express();
+app.use(express.static('public'));
+app.use(express.json({limit: '200mb'})); 
+app.use(cors());
 
 const sendEmail = (recipientEmailAddress, senderEmailAddress, subject, html, fromName = '') => {
     return new Promise((resolve, reject) => {
@@ -57,4 +69,18 @@ const sendEmail = (recipientEmailAddress, senderEmailAddress, subject, html, fro
  Testing <b>SMTP.com</b>
  `
 
- sendEmail('admin@pymnts.com', 'admin@pymnts.com', 'Testing SMTP.com', email, 'admin_pymnts_com299', "PYMNTS");
+ //sendEmail('admin@pymnts.com', 'admin@pymnts.com', 'Testing SMTP.com', email, 'admin_pymnts_com299', "PYMNTS");
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+const httpsServer = https.createServer({
+    key: fs.readFileSync(privateKeyPath),
+    cert: fs.readFileSync(fullchainPath),
+  }, app);
+  
+
+  httpsServer.listen(listenPort, () => {
+    console.log(`HTTPS Server running on port ${listenPort}`);
+});
